222e2badf0f33e5fadc0a283e6d8aa509d37d8a4
C6WX <callumw20060@gmail.com>
2024-10-15 14:48:49 +0100

    added reloading

--- Unity/Assets/Scripts/UI/AmmoCount.cs
+++ Unity/Assets/Scripts/UI/AmmoCount.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AmmoCount : MonoBehaviour
 {
+    TMP_Text ammoCountText;
+    Shooting shootingScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ammoCountText = GetComponent<TMP_Text>();
+        shootingScript = GameObject.FindObjectOfType<Shooting>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ammoCountText.text = (shootingScript.ammoCount.ToString() + "/" + shootingScript.maxAmmo.ToString());
     }
 }
